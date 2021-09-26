37c248b8787fd68c33012bb5a7d7b3ae4d2cd1ca
benroywillis <benroywillis@gmail.com>
2021-09-26 10:35:33 -0700

    Added GEMM Halide program but there's a runtime error

---
+++ GEMM/Halide/GEMM_generate.cpp
@@ -0,0 +1,30 @@
+#include "Halide.h"
+#include "halide_trace_config.h"
+#include "halide_image_io.h"
+#include <math.h>
+
+#define M_PIf	(float)M_PI
+
+namespace {
+
+class GeneralizedMatrixMultiply : public Halide::Generator<GeneralizedMatrixMultiply> {
+public:
+    Input<Buffer<double>> input0{"input0", 2};
+    Input<Buffer<double>> input1{"input1", 2};
+    Output<Buffer<double>> GEMM{"GEMM", 2};
+
+    void generate() {
+    	Var i("i"), j("j"), k("k");
+		Func inner_loop("inner_loop");
+		inner_loop(k, i, j) = input0(i, k)*input1(k, j);
+
+		RDom rv(0, input0.width());
+		Func output("output");
+		output(i, j) += inner_loop(rv, i, j);
+		GEMM = output;
+    }
+};
+
+}  // namespace
+
+HALIDE_REGISTER_GENERATOR(GeneralizedMatrixMultiply, GEMM)
---
+++ GEMM/Halide/GEMM_run.cpp
@@ -0,0 +1,56 @@
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+//#include "GEMM.h"
+//#ifndef NO_AUTO_SCHEDULE
+//#include "GEMM_autoschedule_true_generated.h"
+#include "GEMM_autoschedule_false_generated.h"
+//#endif
+
+#include "HalideBuffer.h"
+#include "halide_benchmark.h"
+#include "halide_image_io.h"
+
+#define SIZE 		512
+#define PRECISION	double
+
+using namespace Halide::Tools;
+using namespace Halide::Runtime;
+
+int main(int argc, char **argv) {
+    int timing_iterations = 15;
+	//PRECISION* A = (PRECISION*)malloc(SIZE*SIZE*sizeof(PRECISION));
+	//PRECISION* B = (PRECISION*)malloc(SIZE*SIZE*sizeof(PRECISION));
+	//PRECISION* C = (PRECISION*)calloc(SIZE*SIZE,sizeof(PRECISION));
+	PRECISION A[SIZE][SIZE];
+	PRECISION B[SIZE][SIZE];
+	PRECISION C[SIZE][SIZE];
+	for( unsigned int i = 0; i < SIZE; i++ )
+	{
+		for( unsigned int j = 0; j < SIZE; j++ )
+		{
+			//*(A + i*SIZE + j) = (PRECISION)rand();
+			//*(B + i*SIZE + j) = (PRECISION)rand();
+			A[i][j] = (PRECISION)rand();
+			B[i][j] = (PRECISION)rand();
+		}
+	}
+	// min index, max index, stride
+	halide_dimension_t arrayDims;
+	arrayDims.min  = 0;
+	arrayDims.extent = SIZE;
+	arrayDims.stride = 1;
+    Buffer<double> input0( &A[0][0], 2, &arrayDims );
+    Buffer<double> input1( &B[0][0], 2, &arrayDims );
+    Buffer<double> output( &C[0][0], 2, &arrayDims );
+
+    // Manually-tuned version
+    double min_t_manual = benchmark(timing_iterations, 10, [&]() {
+        GEMM_autoschedule_false_generated(input0, input1, output);
+        output.device_sync();
+    });
+    printf("Manually-tuned time: %gms\n", min_t_manual * 1e3);
+    return 0;
+}
