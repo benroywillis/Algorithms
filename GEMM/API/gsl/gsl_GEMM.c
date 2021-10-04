7c1c10f6729e27c801dfdca452448e40dea819e9
benroywillis <benroywillis@gmail.com>
2021-10-04 13:15:38 -0700

    Added timing code

--- GEMM/API/gsl/gsl_GEMM.c
+++ GEMM/API/gsl/gsl_GEMM.c
@@ -3,9 +3,12 @@
 #include <stdio.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_blas.h>
+#include <time.h>
 
 int main(int argc, char** argv)
 {
+  struct timespec start, end;
+  while( clock_gettime(CLOCK_MONOTONIC, &start) ) {}
   if( argc < 6 || argc > 7 )
   {
     printf("This application computes alpha*A*B + beta*C (BLAS L3). Please provide a, b, c matrix dimensions (axb*bxc), alpha and beta coefficients (in that order), and 0 or 1 (set matrix transpose to false or true)\n");
@@ -39,6 +42,10 @@ int main(int argc, char** argv)
   gsl_matrix_free(A);
   gsl_matrix_free(B);
   gsl_matrix_free(C);
-
+  while( clock_gettime(CLOCK_MONOTONIC, &end) ) {}
+  double diff = (double)end.tv_sec - (double)start.tv_sec;
+  double ndiff = ((double)end.tv_nsec - (double)start.tv_nsec) * pow(10.0, -9.0);
+  double total = diff + ndiff;
+  printf("Time: %fs\n", total);
   return 0;
 }
