87084b2bec52b01b4473ce0ac8cfaf3ad386ded3
benroywillis <benroywillis@gmail.com>
2021-10-19 16:41:48 -0700

    Got rid of PERFECT files, it was a dead end. Modified environment to use frontier of tool backend

--- Environment.mk
+++ Environment.mk
@@ -1,6 +1,8 @@
 LLVM_INSTALL=/mnt/heorot-10/bwilli46/LLVM9/install-release/
 #LLVM_INSTALL=/mnt/heorot-10/bwilli46/LLVM12/install-release/
-TRACEATLAS_ROOT=/home/bwilli46/Install/TraceAtlas_dev/
+#TRACEATLAS_ROOT=/home/bwilli46/Install/TraceAtlas_dev/
+TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build/
+#TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build_RelWithDebInfo/
 TRACEATLAS_HC_ROOT=/home/bwilli46/Install/TraceAtlas_hotCode/
 
 # Halide install
