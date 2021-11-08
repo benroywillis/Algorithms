2e55059f0c9286e54e57548111fb7cbe6176db72
benroywillis <benroywillis@gmail.com>
2021-11-08 08:09:06 -0700

    Completed SAR/bp in Halide

--- SAR/Halide/bp/bp_generate.cpp
+++ SAR/Halide/bp/bp_generate.cpp
@@ -13,7 +13,7 @@ public:
 	// input is pulses, upsampled, complex
     Input<Buffer<double>> data{"data", 3};
 	// parameter for something
-    Input<Buffer<double>> platpos{"platpos", 1};
+    Input<Buffer<double>> platpos{"platpos", 2};
 	// parameter for something
     Input<Buffer<double>> ku{"ku", 1};
 	// parameter for something
@@ -32,15 +32,15 @@ public:
 		Var x("x"), y("y"), p("p"), c("c");
 		// clamps induction variables to input array bounds
 		Func input("input");
-		input(x, y, c) = data( clamp(x, 0, data.height()), clamp(y, 0, data.width()-1), clamp(c, 0, 2) );
+		input(x, y, c) = data( clamp(x, 0, data.height()-1), clamp(y, 0, data.width()-1), clamp(c, 0, 2) );
 
 		// compute diffs between the current coordinates and the camera position
 		Func xdiff("xdiff");
-		xdiff(x, p) = platpos(3*p) - (-BP_NPIX_X / 2.0f + 0.5f - cast<float>(x)) * dxdy(0);
+		xdiff(x, p) = platpos(p, 0) - (-BP_NPIX_X / 2.0f + 0.5f - cast<float>(x)) * dxdy(0);
 		Func ydiff("ydiff");
-		ydiff(y, p) = platpos(3*p+1) - (-BP_NPIX_Y / 2.0f + 0.5f - cast<float>(y)) * dxdy(0);
+		ydiff(y, p) = platpos(p, 1) - (-BP_NPIX_Y / 2.0f + 0.5f - cast<float>(y)) * dxdy(0);
 		Func zdiff("zdiff");
-		zdiff(p) = platpos(3*p+2) - (-BP_NPIX_Y / 2.0f + 0.5f - z0(0)) * dxdy(0);
+		zdiff(p) = platpos(p, 2) - (-BP_NPIX_Y / 2.0f + 0.5f - z0(0)) * dxdy(0);
 		Func R("R");
 		R(x, y, p) = sqrt( xdiff(x, p)*xdiff(x, p) + ydiff(y, p)*ydiff(y, p) + zdiff(p)*zdiff(p) );
 		Func bin("bin");
@@ -64,15 +64,17 @@ public:
 		Func mul_sample_filter_im("mul_sample_filter_im");
 		mul_sample_filter_im(x, y, p) = compute_sample_re(x, y, p)*matched_filter_im(x, y, p) + compute_sample_im(x, y, p)*matched_filter_re(x, y, p);
 
-		// reduction domain over the space of pulses
-		RDom r(0, N_PULSES);
 		// Function determines if we accumulate the current pulse or not (bin must fall within the correct range)
 		Func accum_re("accum_re");
 		accum_re(x, y, p) = mux( (0 <= bin(x, y, p)) && (bin(x, y, p) < N_RANGE_UPSAMPLED-2), { cast<float>(mul_sample_filter_re(x, y, p)), 0.0f } );
 		Func accum_im("accum_im");
 		accum_im(x, y, p) = mux( (0 <= bin(x, y, p)) && (bin(x, y, p) < N_RANGE_UPSAMPLED-2), { cast<float>(mul_sample_filter_im(x, y, p)), 0.0f } );
+		// reduction domain over the space of pulses
+		RDom r(0, N_PULSES);
+		// initialize each pixel to 0
+		image(x, y, c) = Halide::ConciseCasts::f64(0.0f);
 		// write output
-		image(x, y, c) = mux( c, {accum_re(x, y, r), accum_im(x, y, r)} );
+		image(x, y, c) = mux( c, {cast<double>(accum_re(x, y, r)), cast<double>(accum_im(x, y, r))} );
 	}
 };
 
