24b4b6c0c66b1f035793ca81c495329444e6ea65
benroywillis <benroywillis@gmail.com>
2021-12-08 07:04:09 -0700

    Some new paths and flags for buildflow

--- Environment.mk
+++ Environment.mk
@@ -3,13 +3,16 @@ LLVM_INSTALL=/mnt/heorot-10/bwilli46/LLVM9/install-release/
 #TRACEATLAS_ROOT=/home/bwilli46/Install/TraceAtlas_dev/
 TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build/
 #TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build_RelWithDebInfo/
+#TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build_Release/
 TRACEATLAS_HC_ROOT=/home/bwilli46/Install/TraceAtlas_hotCode/
 
 # Halide install
-#LLVM_HALIDE_INSTALL=/mnt/heorot-10/bwilli46/LLVM12/install-release/
+# Halide 12 install
+#HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Halide-install-release/
 HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Halide10-install-release/
-#HALIDE_COMPILE_ARGS=-g3 -std=c++11 -fno-rtti
-HALIDE_COMPILE_ARGS=-g3 -fno-rtti
+#HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Halide10-install-debug/
+#HALIDE_COMPILE_ARGS=-std=c++11 -fno-rtti
+HALIDE_COMPILE_ARGS=-fno-rtti
 HALIDE_D_LINKS=-lpthread -ldl -lpng -ljpeg
 HALIDE_INCLUDE=\
 -I$(HALIDE_INSTALL_PREFIX)include/\
