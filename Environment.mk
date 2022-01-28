e204b49cff753fae6a47281f885b734ddff50162
benroywillis <benroywillis@gmail.com>
2022-01-28 10:46:52 -0700

    changed environment to use new TraceAtlas with LLVM12 and new installs on heorot-10

--- Environment.mk
+++ Environment.mk
@@ -1,16 +1,13 @@
-LLVM_INSTALL=/mnt/heorot-10/bwilli46/LLVM9/install-release/
-#LLVM_INSTALL=/mnt/heorot-10/bwilli46/LLVM12/install-release/
-#TRACEATLAS_ROOT=/home/bwilli46/Install/TraceAtlas_dev/
-TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build/
-#TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build_RelWithDebInfo/
-#TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build_Release/
-TRACEATLAS_HC_ROOT=/home/bwilli46/Install/TraceAtlas_hotCode/
+#LLVM_INSTALL=/mnt/heorot-10/bwilli46/Installs/LLVM9/install-release/
+LLVM_INSTALL=/mnt/heorot-10/bwilli46/Installs/LLVM12/install-release/
+#TRACEATLAS_ROOT=/home/bwilli46/TraceAtlas/build/
+TRACEATLAS_ROOT=/mnt/heorot-10/bwilli46/Installs/TraceAtlas/devb_relwithdebinfo/
 
 # Halide install
 # Halide 12 install
-#HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Halide-install-release/
-HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Halide10-install-release/
-#HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Halide10-install-debug/
+#HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Installs/Halide-install-release/
+HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Installs/Halide10-install-release/
+#HALIDE_INSTALL_PREFIX=/mnt/heorot-10/bwilli46/Installs/Halide10-install-debug/
 #HALIDE_COMPILE_ARGS=-std=c++11 -fno-rtti
 HALIDE_COMPILE_ARGS=-fno-rtti
 HALIDE_D_LINKS=-lpthread -ldl -lpng -ljpeg
