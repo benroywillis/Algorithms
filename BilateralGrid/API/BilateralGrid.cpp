64b20035dcb36b46b99655d76a077804bbe5f584
benroywillis <benroywillis@gmail.com>
2022-07-31 16:07:23 -0700

    OpenCV bilateral grid

---
+++ BilateralGrid/API/BilateralGrid.cpp
@@ -0,0 +1,28 @@
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/ximgproc/edge_filter.hpp"
+#include "TimingLib.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+	setNumThreads(0);
+	double sigma_s = std::stof(argv[1]);
+	double sigma_r = std::stof(argv[2]);
+	Mat src = imread( argv[3], IMREAD_COLOR );
+	Mat gra; cvtColor(src, gra, COLOR_BGR2GRAY);
+	Mat dst;
+
+	// actual image (known to the API as "guide" or "reference"), sigma_spatial, sigma_luma, sigma_chroma, lambda, num_iter, max_tol
+  	auto fbs = ximgproc::createFastBilateralSolverFilter( src, sigma_s, sigma_r, sigma_r );
+	__TIMINGLIB_start_time();
+	fbs->filter(src, gra, dst);
+	__TIMINGLIB_end_time();
+	imwrite(argv[4], dst);
+	
+	return 0;
+}
