f4f5bf7a863d403aa8ee11aface27b0fc4c62d0a
benroywillis <benroywillis@gmail.com>
2022-08-09 08:59:51 -0700

    Put new benchmark function in Naive GEMM

--- GEMM/Naive/GEMM.cpp
+++ GEMM/Naive/GEMM.cpp
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include "TimingLib.h"
 
 #ifndef TRACING
 #define TRACING 0
@@ -12,27 +13,11 @@
 #define SIZE 	512
 //const int SIZE = 512;
 
-int main()
+void GEMM(int (*in0)[SIZE], int (*in1)[SIZE], int (*out)[SIZE])
 {
-    int(*in0)[SIZE] = malloc(SIZE * sizeof(int[SIZE][SIZE]));
-    int(*in1)[SIZE] = malloc(SIZE * sizeof(int[SIZE][SIZE]));
-    int(*out)[SIZE] = malloc(SIZE * sizeof(int[SIZE][SIZE]));
-
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            in0[i][j] = rand();
-            in1[i][j] = rand();
-            out[i][j] = 0;
-        }
-    }
-
 #if TRACING
    TraceAtlasMarkovKernelEnter("MatrixMultiply,Outer");
 #endif
-	struct timespec start, end;
-	while( clock_gettime(CLOCK_MONOTONIC, &start) ) {}
     for (int i = 0; i < SIZE; i++)
     {
 #if TRACING
@@ -58,11 +43,25 @@ int main()
 #if TRACING
     TraceAtlasMarkovKernelExit("MatrixMultiply,Outer");
 #endif
-	while( clock_gettime(CLOCK_MONOTONIC, &end) ) {}
+}
 
-	double time_s  = (double)end.tv_sec - (double)start.tv_sec;
-	double time_ns = ((double)end.tv_nsec - (double)start.tv_nsec) * pow( 10.0, -9.0 );
-	double total   = time_s + time_ns;
-    printf("Time: %fs\n", total);
+int main()
+{
+    int(*in0)[SIZE] = (int (*)[SIZE])malloc(SIZE * sizeof(int[SIZE][SIZE]));
+    int(*in1)[SIZE] = (int (*)[SIZE])malloc(SIZE * sizeof(int[SIZE][SIZE]));
+    int(*out)[SIZE] = (int (*)[SIZE])malloc(SIZE * sizeof(int[SIZE][SIZE]));
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            in0[i][j] = rand();
+            in1[i][j] = rand();
+            out[i][j] = 0;
+        }
+    }
+
+	__TIMINGLIB_benchmark(10, 10, [&]{ GEMM(in0, in1, out); });
+
     return 0;
 }
