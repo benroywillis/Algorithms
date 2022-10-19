4ede5f6e3e215934ba63a4d36374c81148d09e8b
benroywillis <benroywillis@gmail.com>
2022-10-19 15:02:08 -0700

    Parameterized OpenCV SIFT algorithm

--- SIFT/API/SIFT.cpp
+++ SIFT/API/SIFT.cpp
@@ -10,25 +10,29 @@ using namespace std;
 
 int main( int argc, char** argv )
 {
-	if( argc != 4 )
+	if( argc != 8 )
 	{
-		cout << "Please specify input image path, output image path, and number of threads" << endl;
+		cout << "Please specify octaves, thresholds, curve threshold, contrast threshold, input image path, output image path, and number of threads" << endl;
 		return EXIT_FAILURE;
 	}
-	setNumThreads(atoi(argv[3]));
-	Mat src = imread( argv[1] );
+	setNumThreads(atoi(argv[7]));
+	int octaves = atoi(argv[1]);
+	int thresholds = atoi(argv[2]);
+	float curve_threshold= strtof(argv[3], NULL);
+	float contrast_threshold = strtof(argv[4], NULL);
+	Mat src = imread( argv[5] );
 	Mat gray;
 	cv::cvtColor(src, gray, COLOR_BGR2GRAY);
 	Mat dst;
 
 	__TIMINGLIB_benchmark( [&]() { 
-		auto sift = SIFT::create();
+		auto sift = SIFT::create( 0, octaves, contrast_threshold, curve_threshold);
 		auto keys = vector<KeyPoint>();
 		sift->detect(gray, keys);
 		drawKeypoints(gray, keys, dst);  
 	} );
 
-	imwrite(argv[2], dst);
+	imwrite(argv[6], dst);
 	
  	return 0;
 }
