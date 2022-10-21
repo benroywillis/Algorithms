94113be9090b4247f9bd5ce4a20e11868748d304
benroywillis <benroywillis@gmail.com>
2022-10-21 16:24:37 -0700

    Changed OpenCV algorithm to better resemble Halide but its output is still not blurry enough

--- IIRBlur/API/IIRBlur.cpp
+++ IIRBlur/API/IIRBlur.cpp
@@ -20,25 +20,35 @@ int main( int argc, char** argv )
 	Mat src = imread( argv[2] );
 	Mat dst;
 	float iirKernel[2][1] = { {1-alpha}, {alpha} };
+	float iirKernel[2][1] = { {1-alpha}, {alpha} };
 	Mat K(1, 2, CV_32FC1, &iirKernel);
-	auto anchor = Point(1, 0);
+	// anchor for going down columns
+	auto anchor_down = Point(1, 0);
+	// anchor for going up columns
+	auto anchor_up = Point(0, 0);
 	//float iirKernel[3][3] = { {0.11f, 0.11f, 0.11f}, {0.11f, 0.11f, 0.11f}, {0.11f, 0.11f, 0.11f} };
 	//Mat K(3, 3, CV_32FC1, &iirKernel);
 
 	// IIR filter takes the previous row and filters it with the current row, along the columns
 	// Algorithm:
 	// 1. filter from 0 to num_rows-1
-	// 2. transpose
-	// 3. do step 1 but from num_rows-1 to 0
-	// 4. transpose again
+	// 2. do step 1 but from num_rows-1 to 0
+	// 3. transpose
+	// 4. do step 1
+	// 5. do step 4 but from num_rows-1 to 0
+	// 6. transpose again
 	// source, dest, output image depth (-1 means same as the input image), filter, filtered point position, pixel offset, border
 	__TIMINGLIB_benchmark( [&]() { 
 		Mat blur0;
-  		filter2D( src, blur0, -1, K, anchor, 0, BORDER_REPLICATE ); // border replicate -> aaa | abc | ccc
-  		blur0 = blur0.t();
+  		filter2D( src, blur0, -1, K, anchor_down, 0, BORDER_REPLICATE ); // border replicate -> aaa | abc | ccc
 		Mat blur1;
-  		filter2D( blur0, blur1, -1, K, anchor, 0, BORDER_REPLICATE );
-  		dst = blur1.t();
+  		filter2D( blur0, blur1, -1, K, anchor_up, 0, BORDER_REPLICATE ); // border replicate -> aaa | abc | ccc
+  		blur1 = blur1.t();
+		Mat blur2;
+  		filter2D( blur1, blur2, -1, K, anchor_down, 0, BORDER_REPLICATE );
+		Mat blur3;
+  		filter2D( blur2, blur3, -1, K, anchor_up, 0, BORDER_REPLICATE );
+  		dst = blur3.t();
 	} );
 
 	imwrite(argv[3], dst);
