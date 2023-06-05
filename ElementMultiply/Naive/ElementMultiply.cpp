722fcd034735472cbefdd71f0b94f96ee1f52896
benroywillis <benroywillis@gmail.com>
2023-06-05 15:50:00 -0700

    Element multiply app for basic grammar test

---
+++ ElementMultiply/Naive/ElementMultiply.cpp
@@ -0,0 +1,44 @@
+
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 		512
+#define PRECISION 	int
+
+int main( int argc, char** argv )
+{
+	PRECISION* a = (PRECISION*)malloc(SIZE*SIZE*sizeof(PRECISION));
+	PRECISION* b = (PRECISION*)malloc(SIZE*SIZE*sizeof(PRECISION));
+	PRECISION* c = (PRECISION*)malloc(SIZE*SIZE*sizeof(PRECISION));
+
+	for( int i = 0; i < SIZE; i++ )
+	{
+		for( int j = 0; j < SIZE; j++ )
+		{
+			a[i*SIZE+j] = rand();
+			b[i*SIZE+j] = rand();
+		}
+	}
+
+	for( unsigned i = 0 ; i < SIZE ; i++ )
+	{
+		for( unsigned j = 0; j < SIZE; j++ )
+		{
+			c[i*SIZE + j] = a[i*SIZE + j]*b[i*SIZE + j];
+		}
+	}
+
+	for( unsigned i = 0; i < SIZE; i++ )
+	{
+		for( unsigned j = 0; j < SIZE; j++ )
+		{
+			if( c[i*SIZE+j] > 0 )
+			{
+				// ensures the optimizer won't do away with our previous work
+				volatile bool doNothing = true;
+			}
+		}
+	}
+
+	return 0;
+}
