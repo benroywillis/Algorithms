a80269d2987e30a1ce7d8f84d7dbfddd859ebe3c
benroywillis <benroywillis@gmail.com>
2023-11-13 11:07:25 -0700

    Wrote a fused implementation of CGEMM

--- CGEMM/Naive/CGEMM.cpp
+++ CGEMM/Naive/CGEMM.cpp
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "TimingLib.h"
+#include <complex.h>
 
 #define PRECISION 	float
 #ifndef SIZE
 #define SIZE 		64
 #endif
-
+/* this is the non-fused CGEMM implementation
 void read_input(PRECISION* a)
 {
 	for( unsigned int i = 0; i < SIZE; i++ )
@@ -53,11 +54,11 @@ void Msub(PRECISION* a, PRECISION* b, PRECISION* c)
 			*(c + i*SIZE + j) = *(a + i*SIZE + j) - *(b + i*SIZE + j);
 		}
 	}
-}
+}*/
 
 int main()
 {
-	PRECISION* a = (PRECISION*)calloc( SIZE*SIZE, sizeof(PRECISION) );
+	/*PRECISION* a = (PRECISION*)calloc( SIZE*SIZE, sizeof(PRECISION) );
 	PRECISION* b = (PRECISION*)calloc( SIZE*SIZE, sizeof(PRECISION) );
 	PRECISION* c = (PRECISION*)calloc( SIZE*SIZE, sizeof(PRECISION) );
 	PRECISION* d = (PRECISION*)calloc( SIZE*SIZE, sizeof(PRECISION) );
@@ -79,14 +80,57 @@ int main()
 		GEMM(b, c, scratch);
 		GEMM(a, d, f);
 		Madd(scratch, f, f);
+	} );*/
+
+	// now do a fused implementation and see if the answer is the same
+	_Complex PRECISION* ab = (_Complex PRECISION*)calloc( SIZE*SIZE, sizeof(_Complex PRECISION) );
+	_Complex PRECISION* cd = (_Complex PRECISION*)calloc( SIZE*SIZE, sizeof(_Complex PRECISION) );
+	_Complex PRECISION* ef = (_Complex PRECISION*)calloc( SIZE*SIZE, sizeof(_Complex PRECISION) );
+	for( unsigned i = 0; i < SIZE; i++ )
+	{
+		for( unsigned j = 0; j < SIZE; j++ )
+		{
+			//ab[i*SIZE+j] = a[i*SIZE+j] + b[i*SIZE+j]*I;
+			//cd[i*SIZE+j] = c[i*SIZE+j] + d[i*SIZE+j]*I;
+			ab[i*SIZE+j] = rand() + rand()*I;
+			cd[i*SIZE+j] = rand() + rand()*I;
+		}
+	}
+	__TIMINGLIB_benchmark( [&] {
+		for( unsigned i = 0; i < SIZE; i++ )
+		{
+			for( unsigned j = 0; j < SIZE; j++ )
+			{
+				for( unsigned k = 0; k < SIZE; k++ )
+				{
+					ef[i*SIZE+j] = ab[i*SIZE+k] * cd[k*SIZE+j];
+				}
+			}
+		}
 	} );
 
-	free(a);
+	/* comparison between unfused naive version and fused version
+	_Complex double diff  = 0.0 + 0.0*I;
+	_Complex double total = 0.0 + 0.0*I;
+	for( unsigned i = 0; i < SIZE; i++ )
+	{
+		for( unsigned j = 0; j < SIZE; j++ )
+		{
+			diff  += cabs(ef[i*SIZE+j] - (e[i*SIZE+j] + f[i*SIZE+j]*I));
+			total += ef[i*SIZE+j] - (e[i*SIZE+j] + f[i*SIZE+j]*I);
+		}
+	}
+	printf("Answer difference: %g%%\n", creal(diff / total) * 100);*/
+
+	/*free(a);
 	free(b);
 	free(c);
 	free(d);
 	free(e);
 	free(f);
-	free(scratch);
+	free(scratch);*/
+	free(ab);
+	free(cd);
+	free(ef);
 	return 0;
 }
