7b7a64d2df1259f484d6e540d69145f4fb6fc851
benroywillis <benroywillis@gmail.com>
2024-03-26 08:58:43 -0700

    Increased readability

--- Jacobi_1D/polybench/jacobi-1d-imper.c
+++ Jacobi_1D/polybench/jacobi-1d-imper.c
@@ -63,13 +63,14 @@ void kernel_jacobi_1d_imper(int tsteps,
   int t, i, j;
 
 #pragma scop
-  for (t = 0; t < _PB_TSTEPS; t++)
-    {
-      for (i = 1; i < _PB_N - 1; i++)
-	B[i] = 0.33333 * (A[i-1] + A[i] + A[i + 1]);
-      for (j = 1; j < _PB_N - 1; j++)
-	A[j] = B[j];
+  for (t = 0; t < _PB_TSTEPS; t++) {
+    for (i = 1; i < _PB_N - 1; i++) {
+	  B[i] = 0.33333 * (A[i-1] + A[i] + A[i + 1]);
+      for (j = 1; j < _PB_N - 1; j++) {
+	    A[j] = B[j];
+	  }
     }
+  }
 #pragma endscop
 
 }
