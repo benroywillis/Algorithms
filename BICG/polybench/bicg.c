7e90ef18747b0684c77e6c79b450a481ff855511
benroywillis <benroywillis@gmail.com>
2024-03-28 16:13:52 -0700

    Added curly brackets to increase readability of polybench for loops

--- BICG/polybench/bicg.c
+++ BICG/polybench/bicg.c
@@ -72,17 +72,16 @@ void kernel_bicg(int nx, int ny,
   int i, j;
 
 #pragma scop
-  for (i = 0; i < _PB_NY; i++)
+  for (i = 0; i < _PB_NY; i++) {
     s[i] = 0;
-  for (i = 0; i < _PB_NX; i++)
-    {
-      q[i] = 0;
-      for (j = 0; j < _PB_NY; j++)
-	{
+  }
+  for (i = 0; i < _PB_NX; i++) {
+    q[i] = 0;
+    for (j = 0; j < _PB_NY; j++) {
 	  s[j] = s[j] + r[i] * A[i][j];
 	  q[i] = q[i] + A[i][j] * p[j];
 	}
-    }
+  }
 #pragma endscop
 
 }
--- GEMM/polybench/gemm.c
+++ GEMM/polybench/gemm.c
@@ -74,13 +74,14 @@ void kernel_gemm(int ni, int nj, int nk,
 
 #pragma scop
   /* C := alpha*A*B + beta*C */
-  for (i = 0; i < _PB_NI; i++)
-    for (j = 0; j < _PB_NJ; j++)
-      {
-	C[i][j] *= beta;
-	for (k = 0; k < _PB_NK; ++k)
-	  C[i][j] += alpha * A[i][k] * B[k][j];
+  for (i = 0; i < _PB_NI; i++) {
+    for (j = 0; j < _PB_NJ; j++) {
+	  C[i][j] *= beta;
+	  for (k = 0; k < _PB_NK; ++k) {
+	    C[i][j] += alpha * A[i][k] * B[k][j];
       }
+    }
+  }
 #pragma endscop
 
 }
