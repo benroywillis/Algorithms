3c6a22bf72afab0b1a7e197915c3ec4b9376765b
benroywillis <benroywillis@gmail.com>
2024-06-27 08:12:17 -0700

    Fixed bug having to do with compliance script and dealing with KG_Halide_Generated folders

--- Compliance.py
+++ Compliance.py
@@ -122,7 +122,7 @@ def outputData(complianceMap, path, op):
 				actualTaskToLabel[op] = {}
 			actualTaskToLabel[op][path] = {}
 			# this maps tasks to their labels
-			if (labelKey[op].get(path) is None) and !(path.contains("KG_Halide_Generated")):
+			if (labelKey[op].get(path) is None) or ("KG_Halide_Generated" in path):
 				print("Warning: path "+path+" was not found in LabelKey.json")
 				continue
 			for label in labelKey[op][path]:
